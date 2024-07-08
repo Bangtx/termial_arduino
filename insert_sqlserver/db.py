3f8ddae7fb047dd933aa44129c86ba7c78703233
bang <bangtran.hha@gmail.com>
2024-07-08 14:13:24 +0700

    re-write fot ydc

--- insert_sqlserver/db.py
+++ insert_sqlserver/db.py
@@ -1,13 +1,20 @@
 import pyodbc
 from dotenv import dotenv_values
+from itertools import groupby
+
 
 config = dotenv_values(".env")
-
-DRIVER = config['DRIVER']
-SERVER = config['SERVER']
-DATABASE = config['DATABASE']
-UID = config['UID']
-PWD = config['PWD']
+#
+# DRIVER = config['DRIVER']
+# SERVER = config['SERVER']
+# DATABASE = config['DATABASE']
+# UID = config['UID']
+# PWD = config['PWD']
+DRIVER = 'SQL Server Native Client 11.0'
+SERVER = '(local)\SQLEXPRESS08'
+DATABASE = 'YDCHeater'
+UID = 'sa'
+PWD = 'Ydc@2022'
 print(config)
 
 
@@ -20,10 +27,34 @@ class DataBase:
         self.conn = pyodbc.connect(f'DRIVER={DRIVER};SERVER={SERVER};DATABASE={DATABASE};UID={UID};PWD={PWD}')
         self.cursor = self.conn.cursor()
 
-    def get_list(self):
-        self.cursor.execute(f'SELECT * FROM {self.table_name}')
+    @staticmethod
+    def get_latest_value(sensor_values):
+        # make sure the data have sorted
+        sorted_data = sorted(sensor_values, key=lambda x: x['machine'])
+        # group by sensor id
+        result = dict()
+
+        for key, value in groupby(sorted_data, key=lambda x: x['machine']):
+            # we have to sorted by id (high to low)
+            value = sorted(list(value), key=lambda x: x['id'], reverse=True)
+            result[f'machine_{key}'] = value[0]['temp'] if value else None
+        return result
+
+    def get_list(self, limit=200):
+        # select top 16 * from temperature   order by id desc
+        self.cursor.execute(f'select top {limit} * from temperature   order by id desc')
+        # self.cursor.execute(f'SELECT * FROM {self.table_name} order by id desc limit {limit}')
         rows = self.cursor.fetchall()
-        return rows
+
+        result = []
+        for row in rows:
+            result.append({
+                'id': row[0],
+                'temp': row[1],
+                'machine': row[2]
+            })
+
+        return result
 
     def insert_one(self, **kwargs):
         # if not kwargs['temp']
--- my_serial.py
+++ my_serial.py
@@ -1,17 +1,45 @@
 from config_environment import BAUDRATE, PORT
-from serial import Serial
+import serial
+import sqlite3
+from insert_sqlserver.db import DataBase
+from itertools import groupby
+import time
+
+SERIAL_PORT = "COM3"  # Replace with your Arduino's serial port
+BAUD_RATE = 115200
 
 
-class AppSerial(Serial):
-    ser = Serial(port=PORT, baudrate=BAUDRATE)
-    ser.open()
+def write_bf(message):
+    with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
+        ser.write(message.encode())  # Send message with newline
 
-    def write(self, data: dict):
-        if not self.ser.is_open:
-            self.ser.open()
-        else:
-            write_data = ''
-            for key, value in data.items():
-                write_data += f'{key}={value}'
-            return super().write(write_data)
-        return None
+        while True:
+            ser.write(message.encode())  # Send message with newline
+            response = ser.readline().decode().strip()
+            if response:
+                print(response)
+                break  # Exit loop after receiving re
+
+
+# get value from sql server
+sql_server = DataBase(table_name='temperature')
+sql_server_data = sql_server.get_list()
+sql_server_data = sql_server.get_latest_value(sql_server_data)
+# print(sql_server_data)
+
+db = sqlite3.connect("setting.db")
+cursor = db.cursor()
+
+data = cursor.execute('select * from alarm_setting').fetchall()
+setting_data = []
+for row in data:
+    machine = row[1]
+    low = row[5]
+    high = row[6]
+
+    if machine in sql_server_data and sql_server_data[machine] is not None:
+        temp = sql_server_data[machine]
+        status = 1 if temp > high else 0
+        write_bf(f'{machine}={status}')
+
+    time.sleep(2)
